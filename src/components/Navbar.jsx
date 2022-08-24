16feca453970e9ffeeee2a68659627f4fb3fdad7
Tommaso Coviello <tommaso.coviello2@gmail.com>
2022-08-24 18:10:25 +0200

    Fixed css error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex flex-row relative items-center justify-between p-4 bg-gray-300">
-      <div className="space-x-8">
+      <div className="flex items-center space-x-8">
         <a href="/" className="">
           <span className="text-3xl font-semibold px-3">DriveBox</span>
         </a>
