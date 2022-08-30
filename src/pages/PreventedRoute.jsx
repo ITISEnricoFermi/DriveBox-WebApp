0edf22a6afb88461e43153ab986b8ac651a07f4d
KovD3v <tommaso.coviello@icloud.com>
2022-08-30 20:03:51 +0200

    Add some comments

--- src/pages/PreventedRoute.jsx
+++ src/pages/PreventedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 import {UserAuth} from '../context/AuthContext'
 
-// Prevent the user from accessing page if they are already logged in
+// Prevent the user from accessing pages if they are already logged in
 const PreventedRoute = ({children}) => {
     const {user} = UserAuth()
     if (user) {
